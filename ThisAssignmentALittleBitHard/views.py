54edb6f53e4188a782a42a649d4154dcc422fd90
uosmantamer <osmntamer@gmail.com>
2017-12-14 21:58:44 +0300

    Update views.py

--- ThisAssignmentALittleBitHard/views.py
+++ ThisAssignmentALittleBitHard/views.py
@@ -9,5 +9,5 @@ def my_details(request, mycar):
     return HttpResponse("<h2>%s is what i want.<h2>"%str(mycar))
 
 def main_page(request):
-    return HttpResponse("<img src='https://memegenerator.net/img/instances/500x/64716536/hey-buddy-how-ya-doin.jpg'><h2>You are in the wrong page.<h2>"
+    return HttpResponse("<img src='https://i.ytimg.com/vi/5qfzqNtfP_Q/hqdefault.jpg'><h2>You are in the wrong page.<h2>"
                         "<h2>type /ThisAssignmentALittleBitHard/(car_brand_here)/</h2>")
